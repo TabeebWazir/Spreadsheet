7842d903e71a91504693061e6c9e8cc17c25d864
PolarProgrammer <polarprogramer@gmail.com>
2024-01-06 15:44:28 -0500

    Currently your range function returns an array with the correct length, but all of the values are the value of start. To fix this, chain the .map() method to your .fill() method.
    Pass the .map() method a callback which takes element and index as parameters and returns the sum of those parameters.

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ window.onload = () => {
   };
 };
 
-const range = (start, end) => Array(end - start + 1).fill(start)
+const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index)
