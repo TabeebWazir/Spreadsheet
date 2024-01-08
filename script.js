34c897d32d741758128bf6cfd1f9376455a4d888
Tabeeb Wazir <readingworld2@gmail.com>
2024-01-08 00:03:24 -0500

    Now in your nested .forEach() call, declare an input variable. Use the .createElement() method of the document object to create an input element. Set the type attribute to text and the id attribute to letter + number.
    For accessibility, set the aria-label attribute to the same value as the id attribute.

--- script.js
+++ script.js
@@ -16,10 +16,17 @@ window.onload = () => {
     label.textContent = name;
     container.appendChild(label);
   };
+
   const letters = charRange("A", "J");
+
   letters.forEach(createLabel);
   range(1, 99).forEach((number) => {
     createLabel(number);
-    letters.forEach((letter) => {});
+    letters.forEach((letter) => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+    });
   });
 };
