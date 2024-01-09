f045176b2451017c11f14be81ac12df891411f96
Tabeeb Wazir <readingworld2@gmail.com>
2024-01-09 12:42:37 -0500

    Now you need to check if the value does not include the id of the element. Create an if condition to do so.

--- script.js
+++ script.js
@@ -57,4 +57,6 @@ window.onload = () => {
 const update = (event) => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
+  if (!value.includes(element.id)) {
+  }
 };
