792a93b969eb1cf57ed5a1691bd7ce6b949a574a
Tabeeb Wazir <readingworld2@gmail.com>
2024-01-27 21:36:56 -0500

    Add a someeven property to your spreadsheetFunctions - use the .some() method to check if any element in the array is even.

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ const spreadsheetFunctions = {
   lasttwo: (nums) => nums.slice(-2),
   has2: (nums) => nums.includes(2),
   increment: (nums) => nums.map((num) => (num += 1)),
+  someeven: (nums) => nums.some((num) => num % 2 === 0),
 };
 
 const applyFunction = (str) => {
